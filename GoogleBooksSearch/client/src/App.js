342de3b19dc5555c55ccd24a5d1ff7e4076a6e49
Stefan Salituro <stefansalituro@gmail.com>
2020-11-08 11:53:28 -0500

    componrntd folders created app.js and index.js imported.

--- GoogleBooksSearch/client/src/App.js
+++ GoogleBooksSearch/client/src/App.js
@@ -1,20 +1,20 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/home";
+import Bookshelf from "./pages/bookshelf";
+import Search from "./pages/search";
+import NavBar from "./components/NavBar/NavBar";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
-      </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+const App = () => (
+  <Router>
+    <div>
+      <NavBar />
+      <Route exact path="/" component={Search} />
+      <Route exact path="/bookshelf" component={Bookshelf} />
+      <Route exact path="/search" component={Search} />
+
     </div>
-  );
-}
-
+  </Router>
+);
 
 export default App;
